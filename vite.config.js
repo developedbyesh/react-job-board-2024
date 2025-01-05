e167a577371d480070ce734caec38a8b287c5753
Imesh Gunasekara <imesh@jrcsoftware.com>
2025-01-05 10:56:32 +0800

    json server

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000', // The backend server
+        changeOrigin: true, // Changes the origin of the request to match the target URL
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
   },
 });
